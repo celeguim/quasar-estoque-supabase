7d49e8da059adcd23e055dffb53eeccb5c62ea04
LuizCeleghin <100628977+LuizCeleghin@users.noreply.github.com>
2022-05-07 14:21:50 +0200

    title

--- quasar.config.js
+++ quasar.config.js
@@ -144,8 +144,8 @@ module.exports = configure(function (ctx) {
       },
 
       manifest: {
-        name: `Quasar App`,
-        short_name: `Quasar App`,
+        name: `Estoque App`,
+        short_name: `Estoque App`,
         description: `A Quasar Project`,
         display: "standalone",
         orientation: "portrait",
